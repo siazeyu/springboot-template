948700768b46d5f60c727e1100a38e9c1a0de868
Siaze <siaze@qq.com>
2022-07-13 01:24:33 +0800

    mybatisplus代码生产器

--- src/main/java/com/szy/SpringbootTemplateApplication.java
+++ src/main/java/com/szy/SpringbootTemplateApplication.java
@@ -1,9 +1,11 @@
 package com.szy;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.szy.mapper")
 public class SpringbootTemplateApplication {
 
     public static void main(String[] args) {
