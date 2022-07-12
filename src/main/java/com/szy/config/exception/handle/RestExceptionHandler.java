af0754572af48aa61abd546da4c5ce5268331a66
Siaze <siaze@qq.com>
2022-07-13 01:24:54 +0800

    取消无用日志

--- src/main/java/com/szy/config/exception/handle/RestExceptionHandler.java
+++ src/main/java/com/szy/config/exception/handle/RestExceptionHandler.java
@@ -21,8 +21,6 @@ public class RestExceptionHandler {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResultData<String> exception(Exception e) {
 
-        log.error("全局异常信息 ex={}", e.getMessage(), e);
-
         return ResultData.fail(ReturnCode.RC500.getCode(),e.getMessage());
 
     }
