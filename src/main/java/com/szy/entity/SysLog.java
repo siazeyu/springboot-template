481c2a8ebefb0cfe6c1afaf9881e3fc638265788
Siaze <siaze@qq.com>
2022-07-13 02:38:13 +0800

    新增字段time时间

--- src/main/java/com/szy/entity/SysLog.java
+++ src/main/java/com/szy/entity/SysLog.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
+import java.util.Date;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -73,5 +75,9 @@ public class SysLog implements Serializable {
      */
     private String data;
 
+    /**
+     * 发生时间
+     */
+    private Date time;
 
 }
