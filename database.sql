e0aa9b47f000ea480cdd42aa4b49d32d6812e120
Siaze <siaze@qq.com>
2022-07-13 02:36:49 +0800

    sql建表脚本

--- database.sql
+++ database.sql
@@ -3,15 +3,17 @@ create table sys_log
 (
     id             bigint auto_increment comment 'id主键'
         primary key,
-    method         varchar(50)   null comment '调用的方法',
-    ip             varchar(20)   null comment '请求者的IP',
-    requestUrl     varchar(80)   null comment '请求的url',
-    params         varchar(100)  null comment '请求参数',
-    request_method varchar(10)   null comment '请求方式（GET、POST...)',
-    request_user   varchar(50)   null comment '请求者',
-    status         tinyint(1)    null comment '过程是否发生异常',
-    err_msg        varchar(200)  null comment '错误信息',
-    data           varchar(1000) null comment '方法返回值'
+    method         varchar(50)                         null comment '调用的方法',
+    ip             varchar(20)                         null comment '请求者的IP',
+    request_url    varchar(80)                         null comment '请求的url',
+    params         varchar(100)                        null comment '请求参数',
+    request_method varchar(10)                         null comment '请求方式（GET、POST...)',
+    request_user   varchar(50)                         null comment '请求者',
+    status         tinyint(1)                          null comment '过程是否发生异常',
+    err_msg        varchar(200)                        null comment '错误信息',
+    data           varchar(1000)                       null comment '方法返回值',
+    time           timestamp default CURRENT_TIMESTAMP null comment '发生时间'
 )
     comment '请求日志表';
+
 
